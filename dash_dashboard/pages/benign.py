f29f4f1ea60adbf3b0a0a0df705a8465b5a9e6db
Martyna Kochalska <pyskaa0506@gmail.com>
2025-10-09 18:55:03 +0200

    Tested csv/html plot display

--- dash_dashboard/pages/benign.py
+++ dash_dashboard/pages/benign.py
@@ -1,16 +1,56 @@
 import dash
 from dash import html, dcc
-import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path="/benign")
 
-df = px.data.gapminder().query("year == 2007")
-fig = px.scatter(
-    df, x="gdpPercap", y="lifeExp", size="pop", color="continent",
-    hover_name="country", log_x=True, size_max=60
+csv_path = "assets/label_counts.csv"
+label_counts_df = pd.read_csv(csv_path)
+
+
+fig = go.Figure()
+
+for _, row in label_counts_df.iterrows():
+    fig.add_trace(go.Bar(
+        x=[row['Label']],
+        y=[row['count']],
+        name=row['Label']
+    ))
+
+fig.update_layout(
+    title_text="Distribution of Traffic Types (Full Dataset)",
+    xaxis_title="Traffic/Attack Type (Label)",
+    yaxis_title="Number of Occurrences",
+    height=600,
+    showlegend=False
 )
 
+
 layout = html.Div([
-    html.H3("Benign"),
-    dcc.Graph(figure=fig)
+    html.H3("Benign Traffic Analysis"),
+
+    dcc.Graph(figure=fig),
+
+
+    dbc.Alert(
+        [
+            html.I(className="bi bi-info-circle-fill me-2"),
+            html.Span(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem "
+                "accusantium doloremque laudantium, totam rem aperiam, eaque ipsa "
+                "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+            )
+        ],
+        color="primary",
+        className="mt-3",
+        style={
+            "font-size": "0.9rem",
+            "background-color": "#eef5ff",
+            "border": "1px solid #cce0ff",
+            "color": "#0d47a1"
+        }
+    )
 ])
--- dash_dashboard/pages/home.py
+++ dash_dashboard/pages/home.py
@@ -6,7 +6,7 @@ dash.register_page(__name__, path="/")
 layout = html.Div([
     html.H3("General Overview of all attacks"),
     html.Iframe(
-        src="/assets/chart.html",
+        src="/assets/attack_sunburst_interactive.html",
         style={"width": "100%", "height": "600px", "border": "none"}
     )
 ])
